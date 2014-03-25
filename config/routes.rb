c8c96ec55c0236f1d69dc6d8dd0bff13af0a611a
jamarin6 <jamarinhidalgo@gmail.com>
2014-03-25 19:18:49 +0000

    devise:(

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Spacemanager::Application.routes.draw do
 
-get '/' => 'site#home'
+  devise_for :users
+get '/' => 'devise#signin'
+
+resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
