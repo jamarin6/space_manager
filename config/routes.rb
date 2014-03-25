312462f9706801951b4f845739e7d669e3c4fced
jamarin6 <jamarinhidalgo@gmail.com>
2014-03-25 16:15:47 +0000

    creating models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Spacemanager::Application.routes.draw do
+
+get '/' => 'site#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
