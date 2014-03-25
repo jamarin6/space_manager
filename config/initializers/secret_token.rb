bdc625700fca256be562fe7be0ddadf483dfdb6b
jamarin6 <jamarinhidalgo@gmail.com>
2014-03-25 14:11:58 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spacemanager::Application.config.secret_key_base = 'e27b0705fc83a94e9b184a8013665c66268069e40f43b05c83d119e289bcae6a323c11fe654bc60347c9f64753e9ba1578fbb66e2431203c7063b31311585b27'
