bced8bb044c0824004cc04683c4147f767c5e102
jamarin6 <jamarinhidalgo@gmail.com>
2014-03-27 17:43:16 +0000

    less a template

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -7,4 +7,33 @@ class SpacesController < ApplicationController
 	def incidences
 		pusts "XXXXXXXXXX"
 	end
+
+	def new
+		@space = current_user.spaces.new
+	end
+
+	def create
+		@space = current_user.spaces.create entry_params
+		redirect_to spaces_path	
+	end
+
+	def entry_params
+		params.require(:space).permit(:name, :start_at, :end_at)
+	end
+
+
+	#def create
+	#	@project = Project.find params[:project_id]
+	#	@entry = @project.entries.new entry_params
+#
+	#	if @entry.save
+	#		flash[:notice] = 'Project created successfully'
+	#		#redirect_to project_entries_path(@project.id) #action: 'index', controller: 'entries', project_id: @project.id
+	#		UserMailer.entry_created(@project).deliver
+	#		redirect_to project_entries_path(@project)
+	#	else
+	#		flash[:alert] = "Project hasn't been created"
+	#		render 'new'
+	#	end
+	#end
 end
