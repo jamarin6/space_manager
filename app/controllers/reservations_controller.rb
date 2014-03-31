4adade1134bc8d7d37fa71d5d4a6fa421de94ccf
jamarin6 <jamarinhidalgo@gmail.com>
2014-03-31 16:43:32 +0000

    release ok, incidences :(

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -47,6 +47,12 @@ class ReservationsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@reservation = Reservation.find(params[:id])
+		@reservation.destroy
+		redirect_to spaces_path
+	end
+
 	private
 	def reservation_params
 		params.require(:reservation).permit(:customer, :space_id, :hour, :date, :incidences)
