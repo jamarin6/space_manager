c39a46ab9f4a040a27f925fadb56e891867ba6ce
JoseA <jamarinhidalgo@gmail.com>
2015-01-26 10:54:04 +0100

    añadimos secret key en initializers

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,6 +15,8 @@ Devise.setup do |config|
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
+  # para solventar error en deploy heroku
+  config.secret_key = 'secretkey'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
